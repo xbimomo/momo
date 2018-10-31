df331f0bac23d9ce24b96af1884ee55b0b8f466d
xbimomo <44424833+xbimomo@users.noreply.github.com>
2018-10-31 13:42:58 +0800

    change1
    修改了一行hi world 該你咯

--- main.lua
+++ main.lua
@@ -10,3 +10,6 @@ world.y = display.contentCenterY - 30
 
 local msgText = display.newText( "Hello, world!", world.x, world.y+160, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
+
+local msgText = display.newText( "Hi, world!", world.x, world.y+180, native.systemFont, 32 )
+msgText:setFillColor( 0.7, 0.7, 0.7 )
